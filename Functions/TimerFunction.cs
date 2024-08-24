89083358397f2f816bfc0bbd5569dec158ba70f7
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-08-24 16:38:58 +0100

    just making it so the timer trigger doesn't always run

--- Functions/TimerFunction.cs
+++ Functions/TimerFunction.cs
@@ -1,27 +1,27 @@
-using System;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
+//using System;
+//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.Extensions.Logging;
 
-namespace Sql.Functions
-{
-    public class TimerFunction
-    {
-        private readonly ILogger _logger;
+//namespace Sql.Functions
+//{
+//    public class TimerFunction
+//    {
+//        private readonly ILogger _logger;
 
-        public TimerFunction(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<TimerFunction>();
-        }
+//        public TimerFunction(ILoggerFactory loggerFactory)
+//        {
+//            _logger = loggerFactory.CreateLogger<TimerFunction>();
+//        }
 
-        [Function("TimerFunction")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
-        {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+//        [Function("TimerFunction")]
+//        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+//        {
+//            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             
-            if (myTimer.ScheduleStatus is not null)
-            {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-            }
-        }
-    }
-}
+//            if (myTimer.ScheduleStatus is not null)
+//            {
+//                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+//            }
+//        }
+//    }
+//}
